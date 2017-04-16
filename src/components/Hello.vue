f9beb4105dcdf3a4247999f08c8c3faa1f58870c
EudesSilva <EudesSilva@git>
2017-04-16 17:05:53 -0300

    initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <div>
+      <ul>
+          <li v-for="user in users">
+           {{user.firstName}} {{user.lastName}}
+          </li> 
+      </ul>
+    </div>
+
+  <hr />
+     <div>
+        <input type="text" v-model="digitValue">
+    </div>
+    <div>
+        Digit Value: <span v-text="digitValue" />
+    </div> 
+    <hr />
+       <div>
+           <button v-on:click="numberClicks++">Click number {{numberClicks}}</button>
+       </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+
+  data () {
+    return {
+      msg: 'Welcome to Your Vue',
+
+      users:[
+              { firstName:'Maria', lastName:'souza' },
+              { firstName:'Jose', lastName:'maria' }
+           ],
+
+      digitValue:'',
+
+      numberClicks:0
+    }
+  }
+}
+</script>
+
+
+<style scoped> 
+</style>
